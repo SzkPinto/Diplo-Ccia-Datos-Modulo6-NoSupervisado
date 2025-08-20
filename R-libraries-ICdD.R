3d9e853ef308819ffc519ccc20c401108028991c
David Ruiz <david.ruizj@ciencias.unam.mx>
2025-08-20 14:58:23 -0600

    Add files via upload

---
+++ R-libraries-ICdD.R
@@ -0,0 +1,59 @@
+
+library(tidymodels)
+library(tidyverse)
+library(knitr)
+library(kableExtra)
+library(GGally)
+library(psych)
+library(ggplot2)
+library(broom)
+library(Hmisc)
+library(scales)
+library(recipes)
+library(rgl)
+library(plotly)
+library(ggforce)
+library(tidytext)
+library(FactoMineR)  
+library(factoextra)
+#library(ggord)
+#library(autoplot)
+theme_set(theme_bw(16))
+library('rstatix')
+
+library(ggplot2) #visualizaciones
+library(ggExtra) #visualizaciones
+library(corrplot) #Visualizaciones de la matriz de correlación
+library(dplyr) #Operaciones entre DataFrames
+library(magrittr) #Operadores de Asignación
+library(palmerpenguins) #Data Set de los pingüinos
+library(visdat) #Visualizador de los datos Faltantes
+library(skimr) #Para generar métricas
+library(janitor) #Para embellecer o limpiar los DF
+library(moments) #Calcular la Kurtosis y la Skewness
+library(hexbin) # Auxiliar para visualización
+
+library(funModeling)
+library(SmartEDA)
+library(Hmisc)
+library(dplyr)
+library(ggplot2movies)
+
+library(embed)
+
+library(tidyverse)
+library(tidymodels)
+library(modeldata)
+library(embed)
+library(tsne)
+library(uwot)
+library(scattermore)
+library(readr)
+library(Rtsne)
+library(baguette)
+library(discrim)
+library(gganimate)
+library(data.table)
+
+library(polycor)
+library(tidyverse)
